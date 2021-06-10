d9683f2f8bd405e138b618b607b07bd83c463b33
JH <473731589@qq.com>
2021-06-10 17:11:01 +0800

    成功案例部分done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const glide = new Glide('.glide')
 const captionEl = document.querySelectorAll('.slide-caption')
 glide.on(["mount.after","run.after"],()=>{
@@ -19,3 +20,21 @@ glide.on("run.before",()=>{
 })
 
 glide.mount();
+
+const isotope = new Isotope(".cases",{
+    itemSelector: ".case-item",
+    layoutMode: "fitRows"
+})
+
+const filterBtns = document.querySelector(".filter-btns");
+
+filterBtns.addEventListener('click',(e)=>{
+    console.log('111')
+    const {target} = e;
+    const filterOption = target.getAttribute('data-filter')
+    if(filterOption){
+        document.querySelectorAll(".filter-btn.active").forEach(btn=>btn.classList.remove('active'));
+        target.classList.add('active')
+        isotope.arrange({filter:filterOption})
+    }
+})

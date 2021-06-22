c95319caa4b45c1f0465980e418109e8dcd99108
JH <473731589@qq.com>
2021-06-22 10:06:06 +0800

    滚动过渡动画

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const headerEl = document.querySelector('header')
+const scrollToTop = document.querySelector('.scrollToTop')
+
+window.addEventListener('scroll',()=>{
+    let height = headerEl.getBoundingClientRect().height;
+    if((window.pageYOffset - height) > 800){
+        if(!headerEl.classList.contains("sticky")){
+            headerEl.classList.add("sticky")
+        }
+    }else {
+        headerEl.classList.remove("sticky")
+    }
+
+    if(window.pageYOffset > 2000){
+        scrollToTop.style.display = "block"
+    }else {
+        scrollToTop.style.display = "none"
+    }
+})
 
 const glide = new Glide('.glide')
 const captionEl = document.querySelectorAll('.slide-caption')
@@ -37,4 +56,46 @@ filterBtns.addEventListener('click',(e)=>{
         e.target.classList.add('active')
         isotope.arrange({filter:filterOption})
     }
+})
+
+const staggeringOption = {
+    delay:300,
+    distance:"50px",
+    duration:500,
+    easing:"ease-in-out",
+    origin:"bottom"
+}
+ScrollReveal().reveal(".feature",{...staggeringOption,interval:350}); 
+ScrollReveal().reveal(".service-item",{...staggeringOption,interval:350}); 
+
+const dataSectionEl = document.querySelector(".data-section")
+
+ScrollReveal().reveal(".data-section",{
+    beforeReveal: () => {
+        anime({
+            targets:".data-piece .num",
+            innerHTML:el => {
+                return [0,el.innerHTML]
+            },
+            duration:2000,
+            round:1,
+            easing:"easeInExpo"
+        })
+        dataSectionEl.style.backgroundPosition =`center calc(50% - ${dataSectionEl.getBoundingClientRect().bottom / 5}px)`
+    }
+})
+
+
+window.addEventListener("scroll",()=>{
+    const bottom = dataSectionEl.getBoundingClientRect().bottom;
+    const top = dataSectionEl.getBoundingClientRect().top;
+
+    if(bottom >= 0 && top <= window.innerHTML){
+        dataSectionEl.style.backgroundPosition =`center calc(50% - ${bottom / 5}px)`
+    }
+})
+
+const scroll = new SmoothScroll('nav a[href*="#"], .scrollToTop a[href*="#"]',{
+    header:"header",
+    offset:80
 })
